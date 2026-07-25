f5af4d6c49b7afb0bfd6c54fd718cdd9e8979b39
Vaelastraszz <47340421+Vaelastraszz@users.noreply.github.com>
2026-07-25 11:11:26 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    seenNum := make(map[int]int, len(nums))
+
+	for i, num := range nums {
+		
+		if pos_first_num, ok := seenNum[target-num]; ok {
+			return []int{pos_first_num, i}
+		} 
+		seenNum[num] = i
+	}
+	return nil
+}
