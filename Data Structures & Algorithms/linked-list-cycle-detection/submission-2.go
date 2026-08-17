c0a8b49d295f0e1fa7deacc2212b8fd282590c5b
Vaelastraszz <47340421+Vaelastraszz@users.noreply.github.com>
2026-08-17 10:51:57 +0100

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+    
+	if head == nil || head.Next == nil {
+		return false
+	}
+	
+	slow, fast := head, head
+
+	for fast != nil || fast.Next != nil {
+		
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if fast == nil || fast.Next == nil {
+			break
+		}
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
