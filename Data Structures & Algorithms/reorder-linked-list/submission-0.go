5d64e5bfc76016b8bdbc086c697722369521911b
Vaelastraszz <47340421+Vaelastraszz@users.noreply.github.com>
2026-08-18 11:30:20 +0100

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.go
@@ -0,0 +1,39 @@
+func reorderList(head *ListNode) {
+    if head == nil || head.Next == nil {
+        return
+    }
+
+    middle, fast := head, head.Next
+
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        middle = middle.Next
+    }
+
+    // Reverse second half
+    current := middle.Next
+    var node, next *ListNode
+
+    for current.Next != nil {
+        next = middle.Next
+        middle.Next = current.Next
+        node = middle.Next
+        current.Next = node.Next
+        node.Next = next
+    }
+
+	node = middle.Next
+	middle.Next = nil
+	current = head
+
+	for node != nil {
+		next = node.Next
+
+		node.Next = current.Next
+		current.Next = node
+
+		current = node.Next
+		node = next
+	}
+	
+}
