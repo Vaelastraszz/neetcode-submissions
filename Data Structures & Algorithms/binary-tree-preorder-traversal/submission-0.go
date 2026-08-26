23b3693de5f90669e2762c476157a6576b17a0b5
Vaelastraszz <47340421+Vaelastraszz@users.noreply.github.com>
2026-08-26 09:59:41 +0100

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func preorderTraversal(root *TreeNode) []int {
+    
+	res := []int{}
+
+	var dfs func(root *TreeNode)
+
+	dfs = func(root *TreeNode){
+		if root == nil {
+			return
+		}
+		
+		res = append(res, root.Val)
+		
+		dfs(root.Left)
+		dfs(root.Right)
+	}
+
+	dfs(root)
+
+	return res
+}
