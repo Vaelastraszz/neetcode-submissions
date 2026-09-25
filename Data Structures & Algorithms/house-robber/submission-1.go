587c5033bba3ad427ed4b87c9e061b01193f504d
Vaelastraszz <47340421+Vaelastraszz@users.noreply.github.com>
2026-09-25 08:48:45 +0100

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.go
@@ -0,0 +1,24 @@
+func rob(nums []int) int {
+   if len(nums) == 0 {
+	return 0
+   }
+
+   if len(nums) == 1 {
+	return nums[0]
+   }
+
+   n := len(nums)
+
+   dp := make([]int, n)
+
+   dp[0] = nums[0]
+   dp[1] = max(nums[0], nums[1])
+
+   for i:= 2; i < n; i ++ {
+	dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+   }
+
+   return dp[n-1]
+}
+
+
